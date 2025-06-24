2f92655f3906695929c550d5e5bf44f19ff66316
lethienvu <lethienvu2000@gmail.com>
2025-06-25 06:53:31 +0700

    up

--- camModule.js
+++ camModule.js
@@ -45,7 +45,7 @@ const CameraModule = (() => {
                     Trở về
                 </div>
                 <div class="vu-header-right" id="vuSwitchCameraButton">
-                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor"
                         class="bi bi-arrow-repeat" viewBox="0 0 16 16">
                         <path
                             d="M11.534 7h3.932a.25.25 0 0 1 .192.41l-1.966 2.36a.25.25 0 0 1-.384 0l-1.966-2.36a.25.25 0 0 1 .192-.41m-11 2h3.932a.25.25 0 0 0 .192-.41L2.692 6.23a.25.25 0 0 0-.384 0L.342 8.59A.25.25 0 0 0 .534 9" />
@@ -242,7 +242,7 @@ const CameraModule = (() => {
         }
 
         .vu-camera-app-container .vu-header-right {
-            padding: 0.5rem;
+            padding: 0.8rem;
             display: flex;
             border-radius: 2rem;
             justify-content: center;
